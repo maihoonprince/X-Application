8434412283b45e690d87cf94f6d6bfbc60371c97
Prince <maihoonprinx@gmail.com>
2024-04-24 18:46:39 +0530

    created signup, login, logout routes in auth.routes.js file

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,9 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
+router.post("/signup", (req, res) => {
     res.json({
         data: "you hit the signup endpoints",
+    })
+})
+
+router.post("/login", (req, res) => {
+    res.json({
+        data: "you hit the login endpoints",
+    })
+})
+
+router.post("/logout", (req, res) => {
+    res.json({
+        data: "you hit the logout endpoints",
     })
 })
 
