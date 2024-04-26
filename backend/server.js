fadc79935cb0edffd90a00265d6712a51239e9ce
Prince <maihoonprinx@gmail.com>
2024-04-26 10:12:06 +0530

    created user route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
 
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 
 app.listen(PORT, () => {
