198ae1186fe2b2ae42a583d98e708f412b25b782
Prince <maihoonprinx@gmail.com>
2024-04-26 10:01:13 +0530

    completed protected route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+
 
 dotenv.config();
 
@@ -10,6 +13,11 @@ const app = express();
 
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
