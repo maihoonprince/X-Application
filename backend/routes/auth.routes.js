c8747592659d6ba5de8d9c3ca7c61ce196e51fb3
Prince <maihoonprinx@gmail.com>
2024-04-26 09:07:29 +0530

    adding /me routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
+
+router.get("/me", getMe);
 
 router.post("/signup", signup);
 
