1c9c865e319d2ca0aa20229513a04ba74c8a2b73
Prince <maihoonprinx@gmail.com>
2024-04-26 09:09:18 +0530

    adding protected route in /me

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { getMe, login, logout, signup } from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
-router.get("/me", getMe);
+router.get("/me", protectRoute, getMe);
 
 router.post("/signup", signup);
 
