6a690f019310b22929f5b161755f543bede8a583
Prince <maihoonprinx@gmail.com>
2024-04-29 16:48:08 +0530

    started int5egrating frontend and backend

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,6 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
+import { useMutation } from '@tanstack/react-query'; 
 
 const SignUpPage = () => {
 	const [formData, setFormData] = useState({
@@ -16,6 +17,28 @@ const SignUpPage = () => {
 		password: "",
 	});
 
+	const { mutate, isError, isPending, error } = useMutation({
+		mutationFn: async({email, username, fullName, password}) =>{
+			try {
+				const res = await fetch("/api/auth/signup", {
+					method: "POST",
+					header: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({email, username, fullName, password})
+				});
+
+				if(!res.ok) throw new Error("Somthing went wrong");
+				const data = await res.json();
+				if(data.error) throw new Error(data.error);
+				console.log(data);
+				return data;
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	})
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log(formData);
@@ -24,8 +47,6 @@ const SignUpPage = () => {
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
-
-	const isError = false;
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
